5f59fb17b93117be2291b2051663ca3d173a5526
Simon <simon@gutjahrmail.de>
2026-01-30 09:31:16 +0100

    Force periodic redraw on Aplite

--- src/main.c
+++ src/main.c
@@ -32,6 +32,9 @@ static GColor s_bg_trend_color;
 static Layer *s_weather_deg_layer;
 static GColor s_weather_deg_color;
 static char s_weather_unit_char = 0; // 'C' or 'F'
+#if defined(PBL_PLATFORM_APLITE)
+static AppTimer *s_redraw_timer;
+#endif
 // Persistent 1-char + NUL buffers for each foreground slot
 static char s_slot_text[ROWS][5][2];
 static GFont s_font_dseg_30;       // Bold (foreground)
@@ -99,6 +102,10 @@ static void update_heart_rate(void);
 static GRect get_layout_bounds(void);
 static void main_window_appear(Window *window);
 static void app_focus_handler(bool in_focus);
+static void force_redraw_layers(void);
+#if defined(PBL_PLATFORM_APLITE)
+static void periodic_redraw(void *context);
+#endif
 
 #if PBL_API_EXISTS(unobstructed_area_service_subscribe)
 static void unobstructed_change(AnimationProgress progress, void *context);
@@ -805,7 +812,37 @@ static void app_focus_handler(bool in_focus) {
   }
   layout_rows();
   draw_all_rows();
+  force_redraw_layers();
 }
+
+static void force_redraw_layers(void) {
+  for (int i = 0; i < ROWS; i++) {
+    for (int c = 0; c < 5; c++) {
+      if (s_ghost_layers[i][c]) {
+        layer_mark_dirty(text_layer_get_layer(s_ghost_layers[i][c]));
+      }
+      if (s_ghost_hatch_layers[i][c]) {
+        layer_mark_dirty(s_ghost_hatch_layers[i][c]);
+      }
+      if (s_digit_layers[i][c]) {
+        layer_mark_dirty(text_layer_get_layer(s_digit_layers[i][c]));
+      }
+    }
+  }
+  if (s_bg_trend_layer) {
+    layer_mark_dirty(s_bg_trend_layer);
+  }
+  if (s_weather_deg_layer) {
+    layer_mark_dirty(s_weather_deg_layer);
+  }
+}
+
+#if defined(PBL_PLATFORM_APLITE)
+static void periodic_redraw(void *context) {
+  force_redraw_layers();
+  s_redraw_timer = app_timer_register(1000, periodic_redraw, NULL);
+}
+#endif
 
 static void main_window_unload(Window *window) {
   for (int i = 0; i < ROWS; i++) {
@@ -916,6 +953,9 @@ static void init(void) {
   }, NULL);
 #endif
   app_focus_service_subscribe(app_focus_handler);
+#if defined(PBL_PLATFORM_APLITE)
+  s_redraw_timer = app_timer_register(1000, periodic_redraw, NULL);
+#endif
 
   // Messaging
   app_message_register_inbox_received(inbox_received_callback);
@@ -936,6 +976,12 @@ static void deinit(void) {
   unobstructed_area_service_unsubscribe();
 #endif
   app_focus_service_unsubscribe();
+#if defined(PBL_PLATFORM_APLITE)
+  if (s_redraw_timer) {
+    app_timer_cancel(s_redraw_timer);
+    s_redraw_timer = NULL;
+  }
+#endif
 
   fonts_unload_custom_font(s_font_dseg_30);
   if (s_font_dseg_30_reg) fonts_unload_custom_font(s_font_dseg_30_reg);
